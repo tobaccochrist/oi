e7e360dcb4506f870b6f3da1fe85d4c28f1f141a
tobaccochrist <121753588+tobaccochrist@users.noreply.github.com>
2023-01-01 13:19:43 +0100

    Add files via upload

---
+++ ViewModel/MatrizenRechnerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MatrizenRechner.ViewModel
+{
+    // das ist das main View model, welches als data context an das fenster gebunden wird
+    public class MatrizenRechnerViewModel : ViewModelBase
+    {
+        public MatrizenRechner_ViewMainBodyDisplayViewModel MatrizenRechner_ViewMainBodyDisplayViewModel { get; }
+        public MatrizenRechner_ViewNavbarViewModel MatrizenRechner_ViewNavbarViewModel { get; }
+        public ICommand AddMatrixCommand { get; }
+
+        public MatrizenRechnerViewModel()
+        {
+            MatrizenRechner_ViewMainBodyDisplayViewModel = new MatrizenRechner_ViewMainBodyDisplayViewModel();
+            MatrizenRechner_ViewNavbarViewModel = new MatrizenRechner_ViewNavbarViewModel();
+        }
+    }
+}
