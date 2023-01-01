aa067941c98a141b0c7a746bdf0069544bb6f4bb
tobaccochrist <121753588+tobaccochrist@users.noreply.github.com>
2023-01-01 13:21:06 +0100

    Add files via upload

---
+++ Components/MatrizenRechner_ViewNavbar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatrizenRechner.Components
+{
+    /// <summary>
+    /// Interaktionslogik für MatrizenRechner_ViewNavbar.xaml
+    /// </summary>
+    public partial class MatrizenRechner_ViewNavbar : UserControl
+    {
+        public MatrizenRechner_ViewNavbar()
+        {
+            InitializeComponent();
+        }
+    }
+}
